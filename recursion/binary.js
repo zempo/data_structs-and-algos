7dcd49f63cddd7bd1972ff4e490666e3508346c9
zempo <zelenkosolomon@gmail.com>
2019-10-04 00:04:49 -0700

    add binary function

--- recursion/binary.js
+++ recursion/binary.js
@@ -1,13 +1,11 @@
-function binaryConverter(num) {
-  let numStr = num.toString();
-  if (num != Math.floor(num)) return "Please enter a number";
-  if (num < 0) return "Please enter a positive integer";
+function binaryConverter(n, s) {
+  if (n === 0) return s || "0";
 
-  return "bi";
+  return binaryConverter(Math.floor(n / 2), (n % 2) + s);
 }
 
 var t = (function f(n, s) {
   return n === 0 ? s || "0" : f(~~(n / 2), (n % 2) + s);
 })(22, "");
 
-console.log(binaryConverter(22), t);
+console.log(binaryConverter(22, ""), t);
