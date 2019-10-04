dce508f934f426dedd3b142f4d16e1e0b9d01a80
zempo <zelenkosolomon@gmail.com>
2019-10-03 21:07:41 -0700

    update anagrams

---
+++ recursion/anagrams.js
@@ -0,0 +1,26 @@
+function gramans(str) {
+  if (str.length < 2) return str;
+
+  let permutations = [];
+
+  // create each letter prefix, until 'anagramed'
+  for (let i = 0; i < str.length; i++) {
+    let char = str[i];
+
+    // if duplicate
+    if (str.indexOf(char) != i) {
+      continue; // skip it
+    }
+
+    let remainingStr = str.slice(0, i) + str.slice(i + 1, str.length); // concat the prefix and remainder
+
+    for (var subPermutation of gramans(remainingStr)) {
+      permutations.push(char + subPermutation);
+    }
+  }
+  return permutations;
+}
+
+console.time();
+console.log(gramans("east"));
+console.timeEnd();
--- recursion/maze.js
+++ recursion/maze.js
@@ -8,14 +8,35 @@ let maze = [
   [" ", " ", " ", " ", " ", " ", "e"]
 ];
 
-function findTheCheese(grid, row, pos) {
-  let startPos = grid[0][0];
-
-  for (const key of grid[0].keys()) {
-    console.log(key, grid[0][key]);
+const testRow = (row, pos) => {
+  let rowPath = "";
+  if (pos === 0) {
+    ////// starts from left
+    for (let key of row.keys()) {
+      //   console.log(key, row[key]);
+      if (row[key] === "*") {
+        return rowPath;
+      } else if (key === 0) {
+        rowPath += "";
+      } else {
+        rowPath += "R";
+      }
+    }
   }
+};
 
-  console.log(startPos);
+function findTheCheese(grid, row, pos, path) {
+  if (row === grid.length) return "";
+
+  let startRow = grid[0];
+  let startPos = grid[0][0];
+  let path = "";
+
+  let next = testRow(grid[row], pos);
+  path += next;
+  console.log(next);
+
+  return `Escape maze: ${path}`;
 }
 
-console.log(findTheCheese(maze));
+console.log(findTheCheese(maze, 0, 0));
