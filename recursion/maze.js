a35c5981a27d439c2abb5631716bf9fd0a486574
zempo <zelenkosolomon@gmail.com>
2019-10-07 15:27:43 -0700

    solve maze and organization

--- recursion/maze.js
+++ recursion/maze.js
@@ -8,35 +8,129 @@ let maze = [
   [" ", " ", " ", " ", " ", " ", "e"]
 ];
 
-const testRow = (row, pos) => {
-  let rowPath = "";
-  if (pos === 0) {
-    ////// starts from left
-    for (let key of row.keys()) {
-      //   console.log(key, row[key]);
-      if (row[key] === "*") {
-        return rowPath;
-      } else if (key === 0) {
-        rowPath += "";
+const mazeSolver = (maze, y, x, path = "") => {
+  let mazeCopy = maze.map((arr) => [...arr]);
+
+  if (x < mazeCopy[0].length - 1) {
+    if (mazeCopy[y][x + 1] === " " || mazeCopy[y][x + 1] === "e") {
+      mazeCopy[y][x] = "*";
+      let newPath = path + "R";
+      if (mazeCopy[y][x + 1] === "e") {
+        console.log(`Path to the exit: ${newPath}`);
       } else {
-        rowPath += "R";
+        mazeSolver(mazeCopy, y, x + 1, newPath);
+      }
+    }
+  }
+
+  if (x > 0) {
+    if (mazeCopy[y][x - 1] === " " || mazeCopy[y][x - 1] === "e") {
+      mazeCopy[y][x] = "*";
+      let newPath = path + "L";
+      if (mazeCopy[y][x - 1] === "e") {
+        console.log(`Path to the exit: ${newPath}`);
+      } else {
+        mazeSolver(mazeCopy, y, x - 1, newPath);
+      }
+    }
+  }
+
+  if (y < mazeCopy.length - 1) {
+    if (mazeCopy[y + 1][x] === " " || mazeCopy[y + 1][x] === "e") {
+      mazeCopy[y][x] = "*";
+      let newPath = path + "D";
+      if (mazeCopy[y + 1][x] === "e") {
+        console.log(`Path to the exit: ${newPath}`);
+      } else {
+        mazeSolver(mazeCopy, y + 1, x, newPath);
+      }
+    }
+  }
+
+  if (y > 0) {
+    if (mazeCopy[y - 1][x] === " " || mazeCopy[y - 1][x] === "e") {
+      mazeCopy[y][x] = "*";
+      let newPath = path + "U";
+
+      if (mazeCopy[y - 1][x] === "e") {
+        console.log(`Path to the exit: ${newPath}`);
+      } else {
+        mazeSolver(mazeCopy, y - 1, x, newPath);
       }
     }
   }
 };
 
-function findTheCheese(grid, row, pos, path) {
-  if (row === grid.length) return "";
+console.time();
+console.log(mazeSolver(maze, 0, 0));
+console.timeEnd();
 
-  let startRow = grid[0];
-  let startPos = grid[0][0];
-  let path = "";
-
-  let next = testRow(grid[row], pos);
-  path += next;
-  console.log(next);
-
-  return `Escape maze: ${path}`;
+function isAllowed(result) {
+  if (!result || result === "*") {
+    return false;
+  }
+  return true;
 }
 
-console.log(findTheCheese(maze, 0, 0));
+function nextMove(maze, index, move) {
+  const indexCopy = [...index];
+
+  if (move === "R") {
+    indexCopy[1] = indexCopy[1] + 1;
+    const result = maze[indexCopy[0]][indexCopy[1]];
+    return {
+      move: "R",
+      isValid: isAllowed(result),
+      coord: indexCopy,
+      inverse: "L",
+      priority: 1
+    };
+  } else if (move === "L") {
+    indexCopy[1] = indexCopy[1] - 1;
+    const result = maze[indexCopy[0]][indexCopy[1]];
+    return {
+      move: "L",
+      isValid: isAllowed(result),
+      coord: indexCopy,
+      inverse: "R",
+      priority: 3
+    };
+  } else if (move === "D") {
+    indexCopy[0] = indexCopy[0] + 1;
+    if (!maze[indexCopy[0]]) return false;
+    const result = maze[indexCopy[0]][indexCopy[1]];
+    return {
+      move: "D",
+      isValid: isAllowed(result),
+      coord: indexCopy,
+      inverse: "U",
+      priority: 2
+    };
+  } else if (move === "U") {
+    indexCopy[0] = indexCopy[0] - 1;
+    if (!maze[indexCopy[0]]) return false;
+    const result = maze[indexCopy[0]][indexCopy[1]];
+    return {
+      move: "U",
+      isValid: isAllowed(result),
+      coord: indexCopy,
+      inverse: "D",
+      priority: 4
+    };
+  }
+}
+
+function escapeMaze(maze, index = [0, 0], prevMove = null) {
+  if (maze[index[0]][index[1]] === "e") {
+    return "";
+  }
+  let allMoves = ["R", "L", "U", "D"];
+  allMoves = allMoves.filter((move) => move !== prevMove);
+  const results = allMoves.map((move) => nextMove(maze, index, move));
+  const result = results.sort((a, b) => (a.priority > b.priority ? 1 : -1)).find((result) => result.isValid);
+  return result.move + escapeMaze(maze, result.coord, result.inverse);
+}
+
+console.time();
+console.log(escapeMaze(maze));
+console.timeEnd();
--- recursion/zucker-nization.js
+++ recursion/zucker-nization.js
@@ -1,100 +1,61 @@
-const theFacebook = [
-  { id: 1, name: "Zuckerberg", superior: 0 },
-  { id: 2, name: "Schroepfer", superior: 1 },
-  { id: 3, name: "Bosworth", superior: 2 },
-  { id: 4, name: "Steve", superior: 3 },
-  { id: 5, name: "Kyle", superior: 3 },
-  { id: 6, name: "Andra", superior: 3 },
-  { id: 7, name: "Zhao", superior: 2 },
-  { id: 8, name: "Richie", superior: 7 },
-  { id: 9, name: "Sofia", superior: 7 },
-  { id: 10, name: "Jen", superior: 7 },
-  { id: 11, name: "Schrage", superior: 1 },
-  { id: 12, name: "VanDyck", superior: 11 },
-  { id: 13, name: "Sabrina", superior: 12 },
-  { id: 14, name: "Michelle", superior: 12 },
-  { id: 15, name: "Josh", superior: 12 },
-  { id: 16, name: "Swain", superior: 11 },
-  { id: 17, name: "Blanch", superior: 16 },
-  { id: 18, name: "Tom", superior: 16 },
-  { id: 19, name: "Joe", superior: 16 },
-  { id: 20, name: "Sandberg", superior: 1 },
-  { id: 21, name: "Goler", superior: 20 },
-  { id: 22, name: "Eddie", superior: 21 },
-  { id: 23, name: "Julie", superior: 21 },
-  { id: 24, name: "Annie", superior: 21 },
-  { id: 25, name: "Hernandez", superior: 20 },
-  { id: 26, name: "Rowi", superior: 25 },
-  { id: 27, name: "Inga", superior: 25 },
-  { id: 28, name: "Morgan", superior: 25 },
-  { id: 29, name: "Moissinac", superior: 20 },
-  { id: 30, name: "Amy", superior: 29 },
-  { id: 31, name: "Chuck", superior: 29 },
-  { id: 32, name: "Vinni", superior: 29 },
-  { id: 33, name: "Kelley", superior: 20 },
-  { id: 34, name: "Eric", superior: 33 },
-  { id: 35, name: "Ana", superior: 33 },
-  { id: 36, name: "Wes", superior: 33 }
-];
-
-function processChart(output) {
-  //   console.dir(output, { depth: null });
-
-  return output;
-}
-
-function getNestedChart(members, superior) {
-  var org = [];
-  for (let i in members) {
-    if (members[i].superior == superior) {
-      let subordinates = getNestedChart(members, members[i].id);
-      if (subordinates.length) {
-        members[i].subordinates = subordinates;
+const members = {
+  Zuckerberg: {
+    Schroepfer: {
+      Bosworth: {
+        Steve: "Steve",
+        Kyle: "Kyle",
+        Andra: "Andra"
+      },
+      Zhao: {
+        Richie: "Richie",
+        Sofia: "Sofia",
+        Jen: "Jen"
       }
-      org.push(members[i]);
+    },
+    Schrage: {
+      VanDyck: {
+        Sabrina: "Sabrina",
+        Michelle: "Michelle",
+        Josh: "Josh"
+      },
+      Swain: {
+        Blanch: "Blanch",
+        Tom: "Tom",
+        Joe: "Joe"
+      }
+    },
+    Sandberg: {
+      Goler: {
+        Eddie: "Eddie",
+        Julie: "Julie",
+        Annie: "Annie"
+      },
+      Hernandez: {
+        Rowi: "Rowi",
+        Inga: "Inga",
+        Morgan: "Morgan"
+      },
+      Moissinac: {
+        Amy: "Amy",
+        Chuck: "Chuck",
+        Vinni: "Vinni"
+      },
+      Kelley: {
+        Eric: "Eric",
+        Ana: "Ana",
+        Wes: "Wes"
+      }
     }
   }
-  return processChart(org);
-  //   return JSON.stringify(org, null, 1);
+};
+
+function printHeirarchy(heirarchy, indent = "") {
+  for (const key in heirarchy) {
+    console.log(`${indent}${key}`);
+    if (typeof heirarchy[key] === "object") {
+      printHeirarchy(heirarchy[key], indent + "   ");
+    }
+  }
 }
 
-console.time();
-console.log(getNestedChart(theFacebook, 0));
-console.timeEnd();
-
-// Zuckerberg
-//     Schroepfer
-//         Bosworth
-//             Steve
-//             Kyle
-//             Andra
-//         Zhao
-//             Richie
-//             Sofia
-//             Jen
-//     Schrage
-//         VanDyck
-//             Sabrina
-//             Michelle
-//             Josh
-//         Swain
-//             Blanch
-//             Tom
-//             Joe
-//     Sandberg
-//         Goler
-//             Eddie
-//             Julie
-//             Annie
-//        Hernandez
-//             Rowi
-//             Inga
-//             Morgan
-//        Moissinac
-//             Amy
-//             Chuck
-//             Vinni
-//        Kelley
-//             Eric
-//             Ana
-//             Wes
+printHeirarchy(members);
