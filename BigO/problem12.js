236bed65ac6fb38c2d108a5b441800ffb844acc7
zempo <zelenkosolomon@gmail.com>
2019-10-15 10:30:10 -0700

    add reverse string

--- BigO/problem12.js
+++ BigO/problem12.js
@@ -1,55 +1,50 @@
 // problem 1
 function iterateSheep(sheepToCount) {
-
-    for (let i = 0; i < sheepToCount; i++) {
-        console.log(`${i + 1}: Another sheep jumps over the fence`);
-    }
-    console.log("All sheep jumped over the fence");
+  for (let i = 0; i < sheepToCount; i++) {
+    console.log(`${i + 1}: Another sheep jumps over the fence`);
+  }
+  console.log("All sheep jumped over the fence");
 }
 
-iterateSheep(3)
-console.log('\n')
+iterateSheep(3);
+console.log("\n");
 
 // problem 2
 function iterativePower(base, exp) {
-    if (exp <= 0) return "exponent should be >= 0";
+  if (exp <= 0) return "exponent should be >= 0";
 
-    if (exp === 1) return base;
+  if (exp === 1) return base;
 
-    let output = 1
+  let output = 1;
 
-    for (let i = 0; i < exp; i++) {
-        output *= base
-    }
+  for (let i = 0; i < exp; i++) {
+    output *= base;
+  }
 
-    return output
+  return output;
 }
 
 // console.log(iterativePower(1, -1))
 // console.log(iterativePower(1, 100))
 // console.log(iterativePower(2, 1))
-console.log(iterativePower(2, 3))
-console.log('\n')
+console.log(iterativePower(2, 3));
+console.log("\n");
 
 function revStr(str) {
-    if (typeof str === 'string' || typeof str === 'number') {
-        let toString = str.toString()
-        // if (toString.length === 0) return ""
-        let reverseChars = []
-        for (let i = 0; i < toString.length; i++) {
-            reverseChars.unshift(toString[i])
-        }
-
-        let reversedStr = reverseChars.join('')
-        console.log(reversedStr)
-        return reversedStr
-        // can't figure out how to return this -- yet
-    } else {
-        return "Can only reverse string or number"
+  if (typeof str === "string" || typeof str === "number") {
+    let toString = str.toString();
+    if (toString.length === 0) return "";
+    let reversedStr = "";
+    for (let i = toString.length - 1; i >= 0; i--) {
+      reversedStr += toString[i];
     }
 
-    // let reversed = str[str.length - 1];
-    // return reversed + revStr(str.slice(0, str.length - 1));
+    return reversedStr;
+  } else {
+    return "Can only reverse string or number";
+  }
 }
 
-revStr('smile')
+console.log(revStr(null));
+console.log(revStr(45));
+console.log(revStr("smile"));
