02efa220246b13bbeacbd584adb00eb8c89c796b
Solomon Zelenko <zelenkosolomon@gmail.com>
2019-10-17 12:00:22 -0700

    add 2 algos

--- Arrays/array.js
+++ Arrays/array.js
@@ -77,13 +77,21 @@ function main() {
   let arr = new Array();
 
   // Add an item to the array
+  // arr.push(3);
   arr.push(5);
   arr.push(15);
   arr.push(19);
   arr.push(45);
   arr.push(10);
+  arr.pop();
+  arr.pop();
+  arr.pop();
+  arr.pop();
+  arr.pop();
+  arr.push('tauhida')
 
   console.log(arr);
+  console.log(arr.get(0))
 }
 
 main()
